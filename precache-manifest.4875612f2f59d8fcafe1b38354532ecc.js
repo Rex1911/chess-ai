2f0e5e81c9ab5fef94bcb6f97570c470931e3b44
Aryan Likhite <aryanlikhite@gmail.com>
2020-06-21 23:36:39 +0530

    Updates

---
+++ precache-manifest.4875612f2f59d8fcafe1b38354532ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea26bc104e65034b250e81873352f02",
+    "url": "/chess-ai/index.html"
+  },
+  {
+    "revision": "9cc90647d175caa1ca7a",
+    "url": "/chess-ai/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87e12c494635c5adce85",
+    "url": "/chess-ai/static/js/2.e69b8ba9.chunk.js"
+  },
+  {
+    "revision": "f0a0ef5674dc2572e459e1edb61c9840",
+    "url": "/chess-ai/static/js/2.e69b8ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc90647d175caa1ca7a",
+    "url": "/chess-ai/static/js/main.fec70970.chunk.js"
+  },
+  {
+    "revision": "dfdca70f013dc868ac5c",
+    "url": "/chess-ai/static/js/runtime-main.ce350e42.js"
+  }
+]);
